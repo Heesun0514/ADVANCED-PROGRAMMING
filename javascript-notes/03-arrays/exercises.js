ca8582a37463d944e5bd9964d9ac686e2eb90784
Huiseon Yi <heesun0514@gmail.com>
2026-04-08 21:47:24 +0100

    add method

---
+++ javascript-notes/03-arrays/exercises.js
@@ -0,0 +1,12 @@
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+// ADD 'mango' at the end of array
+
+fruits.push('mango');
+
+// Remove the first element in array 
+
+fruits.shift();
+
+// length of array 
+console.log(fruits.legnth)
